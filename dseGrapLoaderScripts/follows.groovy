2ef4969e9b6224325732bcb21966abd6d66aea6a
rjbriody <bob.briody@datastax.com>
2016-09-06 13:14:26 -0700

    minor changes.

--- dseGrapLoaderScripts/follows.groovy
+++ dseGrapLoaderScripts/follows.groovy
@@ -1,11 +1,10 @@
 // Here is an example command that can be used to load the follows edges. Adjust paths and IPs as necessary based on your environment.
-// cd ~/datastax/dse-graph-loader-5.0.0/; ./graphloader ~/workspace/twitter-export/loader/follows.groovy -graph TWITTERVERSE -address localhost
-
+// cd ~/datastax/dse-graph-loader-5.0.1/; ./graphloader ~/datastax/twitter-exporter/dseGrapLoaderScripts/follows.groovy -graph BOBS_TWITTERVERSE -address localhost
 config create_schema: true //This loading script will initialize the schema if it does not already exists
 
 // !!!!!!!!!!!!!!!!!!!!
 // NOTE: Adjust this path based on the location of your follows.json file
-def input = File.json("/data/bobbriody_twitter_export/follows.json");
+def input = File.json("/Users/bobbriody/datastax/twitter-exporter/data/follows.json");
 
 load(input).asEdges {
     label "follows"
--- dseGrapLoaderScripts/user.groovy
+++ dseGrapLoaderScripts/user.groovy
@@ -1,11 +1,10 @@
 // Here is an example command that can be used to load users. Adjust paths and IPs as necessary based on your environment.
-// cd ~/datastax/dse-graph-loader-5.0.0/; ./graphloader ~/workspace/twitter-exporter/loader/user.groovy -graph TWITTERVERSE -address localhost
-
+// cd ~/datastax/dse-graph-loader-5.0.1/; ./graphloader ~/datastax/twitter-exporter/dseGrapLoaderScripts/user.groovy -graph BOBS_TWITTERVERSE -address localhost
 config create_schema: true //This loading script will initialize the schema if it does not already exists
 
 // !!!!!!!!!!!!!!!!!!!!
 // NOTE: Adjust this path based on the location of your user.json file
-def input = File.json("/data/bobbriody_twitter_export/user.json")
+def input = File.json("/Users/bobbriody/datastax/twitter-exporter/data/user.json")
 
 load(input).asVertices {
     label "user"
